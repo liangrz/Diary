32b8f081720f0e217ac8dd8071765aa6dd061752
liangrz <shasrm@2980.com>
2017-05-10 09:37:54 +0800

    Update and rename 17-04-29-新浪云干掉实名认证框.md to 17-04-29-新浪云干掉实名认证框..js
    rename md to js

--- 17-04-29-新浪云干掉实名认证框..js
+++ 17-04-29-新浪云干掉实名认证框..js
@@ -1,5 +1,5 @@
-#### 1、每5秒消灭一次最后一个Div    
-这个框是通过js的Interval每5秒生成一个Div框，且这个框是最后一个Div，那么直接每5秒消灭这个框就可以了    
+// 1、每5秒消灭一次最后一个Div    
+//这个框是通过js的Interval每5秒生成一个Div框，且这个框是最后一个Div，那么直接每5秒消灭这个框就可以了    
 $(document).ready(function(){    
     function t(){    
       $('div:last').remove();    
@@ -8,9 +8,9 @@ $('div.last').remove();
 var tt = setInterval(t,5000);    
 });    
     
-#### 2、自动触发按键事件
-上述的方法有点瑕疵，由于机器问题可能会有实名认证框一闪而过的踪影    
-这个框有一个停止Interval的事件，就是按右上角的X，让这个事件自动触发就好了    
+// 2、自动触发按键事件
+//上述的方法有点瑕疵，由于机器问题可能会有实名认证框一闪而过的踪影    
+//这个框有一个停止Interval的事件，就是按右上角的X，让这个事件自动触发就好了    
 window.onload = function(){
 	find_kill();
 }
